83e54e3af10be472e059e4c89251a3a5a513ebb1
Cbiltps <1239669254@qq.com>
2025-10-23 00:02:11 +0800

    test

--- LangChain/test.py
+++
@@ -1,1 +0,0 @@
-from langchain_qdrant import QdrantVectorStore, RetrievalMode
--- test.py
+++
@@ -1,6 +0,0 @@
-import mimetypes
-
-content_type, _ = mimetypes.guess_type(
-    "C:\Learning\Code\PythonCode\\temp\mp.weixin.qq.com_Untitled.md"
-)
-print(content_type)
